7db06c1f6b7f4b64a5cd1db7659b62f133e6b691
SlimSider <shadyssider@gmail.com>
2018-02-16 23:57:54 +0100

    Initial commit of easybet

---
+++ src/main/java/edu/slimsider/easybet/EasybetApplication.java
@@ -0,0 +1,12 @@
+package edu.slimsider.easybet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybetApplication.class, args);
+	}
+}
