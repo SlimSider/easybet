eb6b08d2fa13a493ec8ffd36b0a9bc044030cd0c
SlimSider <shadyssider@gmail.com>
2018-03-24 14:29:18 +0100

    Added navigation, updated app.module with the new services and components

--- frontend/easybet-ui/src/app/app.component.ts
+++ frontend/easybet-ui/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {TokenService} from "./service/token.service";
+import {AuthService} from "./service/auth.service";
+import {HttpHeaders} from "@angular/common/http";
+import {UserService} from "./service/user.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  private title = 'app';
+  private user: string;
+  private common = [
+    {link: '/users', title: 'User List'},
+    {link: '/profile/', title: 'User profile'}
+  ];
+
+  constructor(private tokenService: TokenService, private authService: AuthService, private userService: UserService) {
+    console.log(this.tokenService.getToken());
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
