d4050f489e7e5c98740cf93ad1995363495ea824
SlimSider <shadyssider@gmail.com>
2018-03-24 14:27:55 +0100

    Update of User model

--- frontend/easybet-ui/src/app/model/user.ts
+++ frontend/easybet-ui/src/app/model/user.ts
@@ -1,15 +1,22 @@
 export class User {
 
-  constructor(id: number, name: string, password: string, balance: number) {
-    this.id = id;
-    this.name = name;
+  constructor(username?: string, email?: string, password?: string, balance?: number, role?: string) {
+    this.username = username;
+    this.email = email;
     this.password = password;
     this.balance = balance;
+    this.role = role;
   }
 
   private id: number;
-  private name: string;
+  private username: string;
+  private email: string;
   private password: string;
   private balance: number;
+  private role: string;
+
+  getUsername() {
+    return this.username;
+  }
 
 }
