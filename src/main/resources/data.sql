73cb7f4e5b391c84e1dd1d4954a8b4db93936de1
SlimSider <shadyssider@gmail.com>
2018-06-03 14:04:29 +0200

    Update sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO user (id, username, email, password, balance, role) VALUES
   (1, 'admin', 'admin@gmail.com', '$2a$10$tBiHkC6QiuDplBJpSRVxzeiNCZCsbqoOy4GAUD8Xcqi4AKWNUrhgC', 0, 'ADMIN'),
   (2, 'manager', 'manager@gmail.com', '$2a$10$QllpcN7Vigp9uwunTELMK.WTUgpPsDnxD5ZzLb4q97tLQJaWTws2i', 0, 'MANAGER'),
-  (3, 'user', 'user@gmail.com', '$2a$10$FsHrzB44rSuHrDgJBELd0uCr5YiXHLeDIUiuySUH7etsK0gIQwuBa', 10000, 'PLAYER');
+  (3, 'player', 'user@gmail.com', '$2a$10$FsHrzB44rSuHrDgJBELd0uCr5YiXHLeDIUiuySUH7etsK0gIQwuBa', 10000, 'PLAYER');
